aa5ed6d533c0c7c5cd891cf1e41e4122347f3ce9
EvgeniyNikNik <nievni@gmail.com>
2021-09-01 14:00:34 +0300

    fix scv

--- Parser/text_extractor.py
+++ Parser/text_extractor.py
@@ -10,4 +10,4 @@ for file_name in sorted(os.listdir("../PDF_data/astro-ph"))[:200]:
     name_without_extension = file_name[:rpoint]
     parsed = parser.from_file('../PDF_data/astro-ph/' + file_name)
     s = pd.Series([(line if len(line) > 5 else None) for line in parsed["content"].split("\n\n")]).dropna()
-    s.to_csv("./csv_text_data/"+name_without_extension+".scv", index=False)
+    s.to_csv("./csv_text_data/"+name_without_extension+".csv", index=False)
