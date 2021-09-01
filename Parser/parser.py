0d27e8fad06b18371668e47ab288f4f4d1fcc46f
EvgeniyNikNik <nievni@gmail.com>
2021-09-01 17:29:42 +0300

    add html and csv from html files

--- Parser/parser.py
+++ Parser/parser.py
@@ -1,4 +1,4 @@
 from parser_config import *
 from parser_funcs import *
 
-file_names = save_files("/home/stork/PycharmProjects/ClsRecArticles/SCRSA/PDF_data/","astro-ph",CSV_SAVE_PATH)
+file_names = save_files("/home/stork/PycharmProjects/ClsRecArticles/SCRSA/PDF_data/","astro-ph",CSV_SAVE_PATH,num_files=200)
