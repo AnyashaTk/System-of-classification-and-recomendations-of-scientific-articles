59d87ed2841f0182cded2fe7bac20309042abc62
EvgeniyNikNik <nievni@gmail.com>
2021-09-01 11:47:26 +0300

    start w2v experiments

--- page_spliter.py
+++ page_spliter.py
@@ -1,16 +1,14 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import os
-
-for file in os.listdir(
-        "/home/stork/PycharmProjects/System-of-classification-and-recomendations-of-scientific-articles/PDF_data/astro-ph")[
-            :10]:
-    file_path = "/home/stork/PycharmProjects/System-of-classification-and-recomendations-of-scientific-articles/PDF_data/astro-ph/" + file
-    input_PDF = PdfFileReader(open(file_path,
-                                   'rb'))
-    output = PdfFileWriter()
-    new_File_PDF = input_PDF.getPage(0)
-    output.addPage(new_File_PDF)
-    output_Name_File = "./pdf_first_pages" + file
-    outputStream = open(output_Name_File, 'wb')
-    output.write(outputStream)
-    outputStream.close()
+file_names=os.listdir("/home/stork/PycharmProjects/ClsRecArticles/PDF_data/astro-ph")
+for file in file_names[:10]:
+    file_path = "/home/stork/PycharmProjects/ClsRecArticles/PDF_data/astro-ph/" + file
+    with open('/home/stork/PycharmProjects/ClsRecArticles/PDF_data/astro-ph/2101.02454.pdf', 'rb') as f:
+        input_PDF = PdfFileReader(f)
+        output = PdfFileWriter()
+        new_File_PDF = input_PDF.getPage(0)
+        output.addPage(new_File_PDF)
+        output_Name_File = "./pdf_first_pages" + file
+        outputStream = open(output_Name_File, 'wb')
+        output.write(outputStream)
+        outputStream.close()
