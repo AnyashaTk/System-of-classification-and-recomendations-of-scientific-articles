56c8570a58811cb0cfb8eb5e3292853a278f6203
AnyashaTk <47858669+AnyashaTk@users.noreply.github.com>
2021-09-07 12:42:07 +0300

    vocab with all directory csv issue#3
    need new issue, normalize list take long time

--- w2v_clasters/constants.py
+++ w2v_clasters/constants.py
@@ -1,2 +1,6 @@
 
-path = '/home/sparrow/Documents/GitHub/System-of-classification-and-recomendations-of-scientific-articles/csv_text_data/2001.00018.csv'
+# path = '/home/sparrow/Documents/GitHub/System-of-classification-and-recomendations-of-scientific-articles/csv_data/2001.00018.csv'
+path_t = '/home/sparrow/Documents/GitHub/System-of-classification-and-recomendations-of-scientific-articles/csv_text_data/2001.00018.csv'
+path = '/home/sparrow/Documents/GitHub/System-of-classification-and-recomendations-of-scientific-articles/csv_text_data/'
+
+EMB_SIZE = 15
