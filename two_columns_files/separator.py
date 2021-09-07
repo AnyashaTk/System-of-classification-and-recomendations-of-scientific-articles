bc5f53d8a327cbfda16717ab099f0892eb09100c
EvgeniyNikNik <nievni@gmail.com>
2021-09-07 14:08:49 +0300

    add words characteristics

--- two_columns_files/separator.py
+++ two_columns_files/separator.py
@@ -10,16 +10,40 @@ def is_double_columns(line: str) -> bool:
     return False
 
 
-def get_words_from_line(line: str):
+def get_words_from_line(line: str) -> list:
     words = [word if word.strip() != '' else None for word in line.strip().split(" ")]
     while None in words:
         words.remove(None)
     return words
 
 
-SAVE_DIR = "./csv_parsed/"
+def upper_perc(word: str) -> float:
+    upper_count = 0
+    for liter in word:
+        upper_count += liter.isupper()
+    return upper_count / len(word)
+
+
+def lower_perc(word: str) -> float:
+    lower_count = 0
+    for liter in word:
+        lower_count += liter.islower()
+    return lower_count / len(word)
+
+
+def digits_perc(word: str) -> float:
+    digit_count = 0
+    for liter in word:
+        digit_count += liter.isdigit()
+    return digit_count / len(word)
+
+
+SAVE_DIR = "./csv_parsed_by_words/"
 SOURCE_PATH = "./csv_source/"
-COLUMNS_NAME = ["symbolic_percent", "space_percent", "num_world", "num_columns", "front_spaces_perc", "back_spaces"]
+COLUMNS_NAME = ["word", "word_len", "word_upper", "word_lower", "word_digits", "line_symbolic_percent",
+                "line_space_percent",
+                "line_num_world", "line_num_columns",
+                "line_front_spaces_perc", "line_back_spaces"]
 
 file_names = sorted(os.listdir(SOURCE_PATH))
 # нахождение базовых
@@ -31,7 +55,6 @@ for file_name in file_names:
     data_df = pd.DataFrame(
         columns=COLUMNS_NAME)
     for num, line in enumerate(df_text):
-        # данные о словах
         words = get_words_from_line(line)
         # данные о строке
         symbolic_line = line
@@ -42,10 +65,17 @@ for file_name in file_names:
         num_columns = len(line.strip().split("  "))
         front_spaces_perc = df["space_num"][num] / max_line_len
         back_spaces = 1 - len(line) / max_line_len
-        df_line = dict(zip(
-            COLUMNS_NAME, [symbolic_percent, space_percent, num_world, num_columns, front_spaces_perc, back_spaces]))
-        data_df = data_df.append(df_line, ignore_index=True)
+        # получение данных о словах
+        for word in words:
+            word_len = len(word)
+            upper = upper_perc(word)
+            lower = lower_perc(word)
+            digits = digits_perc(word)
+            df_line = dict(zip(
+                COLUMNS_NAME,
+                [word, word_len, upper, lower, digits, symbolic_percent, space_percent, num_world, num_columns,
+                 front_spaces_perc, back_spaces]))
+            data_df = data_df.append(df_line, ignore_index=True)
 
-    df_with_data = df.join(data_df)
     name_without_extension = file_name[:file_name.rfind(".")]
-    df_with_data.to_csv(SAVE_DIR + name_without_extension + "_data.csv", index=False)
+    data_df.to_csv(SAVE_DIR + name_without_extension + "_data.csv", index=False)
